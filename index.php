faea2f0d4edd57d7bc898f7a19815bbd2e9c77d6
KevinAssink <kevin.assink@student.rocvf.nl>
2018-06-06 09:03:24 +0200

    Spotitude

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav class="navbar bg-dark navbar-dark navbar-expand-sm">
+    <div class="navbar-nav">
+        <a class="nav-item nav-link" href="#">SpotiTube</a>
+        <a class="nav-item nav-link" href="#">Home</a>
+        <a class="nav-item nav-link" href="#">Playlists</a>
+    </div>
+</nav>
+<div class="container">
+    <div class="jumbotron">
+        <h1>SpotiTube</h1>
+        <p>Spotitube maakt gebruik van materiaal op YouTube zodat je zowel beeld als muziek kunt afspelen.</p>
+    </div>
+    <h1>Playlist from db</h1>
+    <a href="Rock.html"><img src="img/rock.jpg">
+    </a>
+    <a href=""><img src="img/love_metal_logo_by_heartandvoice-d8cwusl.png">
+    </a>
+</div>
+
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+</body>
+</html>
